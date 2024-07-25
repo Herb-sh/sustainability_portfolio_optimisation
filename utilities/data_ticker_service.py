038cbfe95cf34c6003f8f951dd4407b6c477694d
HerbiShtini <herb_sh@hotmail.com>
2024-07-26 01:14:39 +0200

    Implemented get monthly returns of last 25 years for all companies

--- utilities/data_ticker_service.py
+++ utilities/data_ticker_service.py
@@ -27,18 +27,41 @@ def fetch_market_cap(tickers, batch_size=100, delay=2):
 
     return pd.DataFrame(market_caps)
 
-def get_monthly_returns(tickers, start_date, end_date):
+def get_monthly_returns(tickers, start_date, end_date, interval='1mo'):
     monthly_returns = {}
     for ticker in tickers:
         # Download the stock data
-        data = yf.download(ticker, start=start_date, end=end_date)
+        data = yf.download(ticker, start=start_date, end=end_date, interval=interval, progress=False)
         if data.empty:
             continue
-
         # Resample to monthly data and calculate the monthly returns
-        data['Monthly Return'] = data['Adj Close'].resample('ME').ffill().pct_change()
+        data['Monthly Return'] = (data['Adj Close'].pct_change() * 100).round(2).dropna()
         monthly_returns[ticker] = data['Monthly Return']
 
     # Convert the dictionary to a DataFrame
     monthly_returns_df = pd.DataFrame(monthly_returns)
     return monthly_returns_df
+
+def get_returns_in_chunks(tickers, start_date, end_date, interval='1mo', chunk_size=5, sleep_duration=5):
+    # Initialize an empty DataFrame to store the results
+    all_data = pd.DataFrame()
+
+    # Download data in chunks
+    for chunk in chunks(tickers, chunk_size):
+        print(f"Downloading data for tickers: {chunk}")
+
+        # Download data for the current chunk
+        data = get_monthly_returns(chunk, start_date, end_date, interval)
+        # Append the downloaded data to the all_data DataFrame
+        all_data = pd.concat([all_data, data], axis=1)
+
+        # Pause to avoid overloading the API
+        print("Pausing to avoid overloading the API...")
+        time.sleep(sleep_duration)  # Adjust the sleep time as needed
+
+    # Return the resulting DataFrame
+    return all_data
+
+def chunks(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
