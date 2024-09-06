06a1d1850deabc4f6a165e573f4ee3a32e069d99
HerbiShtini <herb_sh@hotmail.com>
2024-09-07 01:50:53 +0200

    Cleaning & Refactoring

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # sustainability_portfolio_optimisation
+
+
+# Best practice, use an environment rather than install in the base env
+conda create -n portfolio_optimisation
+conda activate portfolio_optimisation
+# If you want to install from conda-forge
+conda config --env --add channels conda-forge
+# The actual install command
+conda install numpy
--- utilities/utility.py
+++ utilities/utility.py
@@ -1,5 +1,46 @@
 import numpy as np
+import pandas as pd
 from scipy.optimize import minimize
+import utilities.variables as variables
+from pypfopt import EfficientFrontier
+
+# Return & Volatility pro years
+# 1, 5, 10, 25 year returns
+# Set average yearly return of the last years (1, 5, 10, 25)
+def set_yearly_return_rates_by_years(df_overview, df_monthly_return):
+    # Loop through time spans
+    for i, years in enumerate(variables.time_span_years):
+        # Loop through tickers/stock name
+        for j, ticker in enumerate(df_overview['stock_ticker_symbol']):
+            # if ticker is found in monthly adjacent columns, meaning there are available data to calculate
+            if ticker in df_monthly_return.columns:
+                # Get date "years" ago
+                date = pd.Timestamp.today() - pd.DateOffset(years=years)
+                # Pick only stocks that are after this date
+                monthly_return_list = df_monthly_return.loc[ pd.to_datetime(df_monthly_return['Date']) >= date, ticker].dropna().tolist()
+                if len(monthly_return_list) >= 2:
+                    # Calculate the i-years total return
+                    total_return = np.prod(monthly_return_list) - 1
+
+                    # Calculate the annualized average return
+                    annualized_return = np.prod(monthly_return_list) ** (1/years)
+                    df_overview.loc[df_overview['stock_ticker_symbol'] == ticker, 'return_rate' + '_' + str(years) + 'y_avg'] = annualized_return
+
+def set_volatility_by_years(df_overview, df_monthly_adj_close):
+    # 1, 5, 10, 25 year returns
+    # Loop through time spans
+    for i, years in enumerate(variables.time_span_years):
+        for i, ticker in enumerate(df_overview['stock_ticker_symbol']):
+            if ticker in df_monthly_adj_close.columns:
+                # Get date "years" ago
+                date = pd.Timestamp.today() - pd.DateOffset(years=years)
+                # Pick only stocks that are after this date
+                adj_close_filtered = df_monthly_adj_close.loc[ pd.to_datetime(df_monthly_adj_close['Date']) >= date, ticker].dropna()
+                std_deviation = adj_close_filtered.pct_change().std()
+
+                if len(adj_close_filtered) >= 2:
+                    df_overview.loc[df_overview['stock_ticker_symbol'] == ticker, 'volatility' + '_' + str(years) + 'y'] = std_deviation
+
 
 # Efficient-Frontier
 def portfolio_performance(weights, returns, volatilities):
@@ -33,5 +74,3 @@ def efficient_frontier(df):
         results.append(result['fun'])
 
     return target_returns, results
-
-#%%
